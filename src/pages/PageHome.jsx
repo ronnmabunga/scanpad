fc5b1011aabf6fa6e9ec207e185eab2886826ca3
ronnmabunga <ronnmabunga@gmail.com>
2025-03-25 21:20:46 +0800

    Update PageHome.jsx

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -11,13 +11,13 @@ function PageHome() {
             <div className="row m-0" style={{ height: "10%" }}>
                 <Navbar bg="dark" variant="dark" expand="lg">
                     <Container>
-                        <Navbar.Brand href="#">OCR-Assisted Text Editor</Navbar.Brand>
+                        <Navbar.Brand href="#">OCR-Assisted Notepad</Navbar.Brand>
                     </Container>
                 </Navbar>
             </div>
             <div className="row m-0" style={{ height: "90%" }}>
                 <div className="col-12 col-sm-6 col-md-7 col-lg-5 col-xl-4 text-center m-0 d-flex flex-column justify-content-center bg-black text-white" style={{ height: "100%", minHeight: "90vh" }}>
-                    <p className="font-body-2">OCR-Assisted Text Editor</p>
+                    <p className="font-body-2">OCR-Assisted Notepad</p>
                     <p className="font-body-4">Paste an image from the clipboard onto this window, after the app transcribes the image, paste again on the editor body to get the transcribed text.</p>
                 </div>
                 <div className="col-12 col-sm-6 col-md-5 col-lg-7 col-xl-8 bg-black text-white py-3" style={{ height: "100%" }}>
